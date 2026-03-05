4fae3458987723843622832205ef5a19325f439d
diego111222333 <lupe.garcia.diego.eduardo>
2026-03-04 21:57:49 -0400

    Mi primer commit de el proyecto de django

---
+++ inicio/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.inicio,name='inicio'),
+    path('perro',views.perro,name='inicio'),
+    path('gato',views.gato,name='gato')
+]
