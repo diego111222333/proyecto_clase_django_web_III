e71803c7e6fae7af2aaa16576e801841ed1fe876
diego111222333 <lupe.garcia.diego.eduardo@itsecsucre.edu.bo>
2026-03-16 11:09:49 -0400

    Delete inicio directory

--- inicio/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.inicio,name='inicio'),
-    path('perro',views.perro,name='inicio'),
-    path('gato',views.gato,name='gato')
-]
--- inicio/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def inicio(request):
-    return HttpResponse('Este es el inicio de la pagina😎😎')
-def perro(request):
-    return HttpResponse("Guau Guau")
-def gato(request):
-    return HttpResponse("Miau Miau")
